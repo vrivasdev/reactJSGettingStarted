bb6b8032b5acd8403d32e5aa60562aa836bc57b0
vrivas <vrivas@teravisiontech.com>
2018-03-14 12:35:51 -0400

    Github Card component reusing components

---
+++ github_card_component.js
@@ -0,0 +1,56 @@
+/* Individual Cards */
+const Card = (props) => {
+	return(
+  	<div style={{margin:"1em"}}>
+  	  <img width="75" src={props.avatar_url}/>
+      <div style={{display:'inline-block', marginLeft:10}}>
+        <div style={{fontSize:'1.25em', fontWeight:'bold'}}>{props.name}</div>
+        <div>{props.company}</div>
+      </div>
+  	</div>
+  );
+};
+
+/* List of Components: Passing values trought each component */
+/*const CardList = (props) => {
+	return(
+  	<div>
+  	  <Card name="Víctor" company="Teravision" avatar_url="https://avatars1.githubusercontent.com/u/8445?v=4"/>
+      <Card name="María" company="Google" avatar_url="https://avatars1.githubusercontent.com/u/8445?v=4"/>
+  	</div>
+  );
+};*/
+
+/*Data retrieved from DB or API*/
+let data = [
+	{
+  	name:'Víctor',
+    company: 'Teravision',
+    avatar_url: 'https://avatars1.githubusercontent.com/u/8445?v=4'
+  },
+  {
+  	name:'María',
+    company: 'Google',
+    avatar_url: 'https://avatars1.githubusercontent.com/u/8445?v=4'
+  }
+];
+
+// Passing data trought one data object to the card component using the "SPREAD OPERATOR"
+const CardList = (props) => {
+	return(
+  <div>
+  	{props.cards.map(card => <Card {... card}/>)}
+	</div>
+  );	
+};
+
+/*// Passing data trought one data object to the card component
+const CardList = (props) => {
+	return(
+  <div>
+	  {props.cards.map(card => <Card name={card.name} company={card.company} avatar_url={card.avatar_url}/>)}
+	</div>
+  );	
+};*/
+
+ReactDOM.render(<CardList cards={data} />, mountNode);

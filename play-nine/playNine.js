4cea21ee1c4019d91a89ac5712cf7ef8baffa4b8
vrivas <vrivas@teravisiontech.com>
2018-04-12 12:24:02 -0400

    Building the Game Interface : The Stars, Button, and Answer Frames

--- play-nine/playNine.js
+++ play-nine/playNine.js
@@ -1,10 +1,58 @@
 
+const Stars = (props) => {
+  return(
+    <div className="col-5">
+      <li className="fa fa-star"></li>
+      <li className="fa fa-star"></li>
+      <li className="fa fa-star"></li>
+      <li className="fa fa-star"></li>
+    </div>
+  );
+}
+
+const Button = (props) => {
+  return(
+    <div className="col-2">
+      <button>=</button>
+    </div>
+  );
+}
+
+const Answer = (props) => {
+  return(
+    <div className="col-5">
+      ...
+    </div>
+  );
+}
+
+const Numbers = (props) => {
+  return(
+    <div className="container">
+      <div className="card text-center">
+        <div>
+          <span>1</span>
+          <span className="selected">2</span>
+          <span className="used">3</span>
+        </div>        
+      </div>
+    </div>
+  );
+}
+
 /* Game component Class */
 class Game extends React.Component{
 	render(){
   	return(
     	<div>
   	  	<h3> Play Nine </h3>
+        <div className="row">
+          <Stars />
+          <Button />
+          <Answer />
+        </div>  
+        <br/>
+        <Numbers />
   		</div>
     );
   }
